3ec00cc694ac5b9c44e59936b7980b8a33a45d93
tirth.kelkar@gmail.com <Katappa@1996>
2022-12-02 13:56:11 -0600

    Init Commit

---
+++ src/test/java/com/tirth/smartcontacts/SmartContactsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tirth.smartcontacts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartContactsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
